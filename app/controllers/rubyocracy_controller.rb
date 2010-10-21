c07857126bdd6981f00d1392ad96fbba4516c5db
yoda <yoda@tallon4.net>
2010-10-21 12:57:56 +0800

    Moved story views to stories

--- app/controllers/rubyocracy_controller.rb
+++ app/controllers/rubyocracy_controller.rb
@@ -2,9 +2,11 @@ class RubyocracyController < ApplicationController
 
   def index
     @stories = Story.ordered.paginate :page => params[:page], :include => :site, :per_page => 20
+    render 'stories/index'
   end
 
   def trending
+    render 'stories/trending_stories'
   end
   
   def hide_notice
